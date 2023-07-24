bcd718f3433d4dd0fce8c6f369707a1d0e15402e
mpcrockett <mpcrockett+github@gmail.com>
2023-07-24 16:50:56 -0400

    fixes to modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,16 +12,16 @@ export default function Modal(props: Props) {
 
   return (
     <>
-    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-      <div className="relative w-auto my-6 mx-auto max-w-3xl">
+    <div className="justify-center items-center flex overflow-scroll fixed inset-0 z-50 outline-none focus:outline-none">
+      <div className="relative w-auto mx-auto max-w-[90%]">
         {/*content*/}
-        <div className="border-0 shadow-lg relative flex flex-col w-full bg-deep-blue outline-none focus:outline-none m-4">
+        <div className="border-0 shadow-lg relative flex flex-col w-full bg-deep-blue outline-none focus:outline-none">
           {/*header*/}
           <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200">
-            <h3 className="text-3xl font-lora font-semibold">
+            <h3 className="text-xl font-lora font-semibold">
               {title}
             </h3>
-            <div className="p-1 ml-auto float-right text-3xl leading-none outline-none focus:outline-none"
+            <div className="p-1 ml-auto float-right leading-none outline-none focus:outline-none"
               onClick={() => setShowModal(false)}
             >
               <BsXLg />
@@ -29,8 +29,8 @@ export default function Modal(props: Props) {
           </div>
           {/*body*/}
 
-          <div className="relative p-6 flex-auto">
-            <p className="my-4 text-white text-lg leading-relaxed font-raleway">
+          <div className="relative flex-auto">
+            <p className="m-4 text-white text-sm font-raleway max-h-[400px] overflow-scroll">
               {description}
             </p>
           </div> 
