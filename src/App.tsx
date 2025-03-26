2fcb24d71a0aeef512c59437cb13bf8256697fc1
mpcrockett <mpcrockett+github@gmail.com>
2025-03-26 17:28:56 -0400

    spacing issue

--- src/App.tsx
+++ src/App.tsx
@@ -12,67 +12,67 @@ import Contact from "./scenes/Contact";
 import { motion } from "framer-motion";
 
 function App() {
-	const [selectedPage, setSelectedPage] = useState<string>("home");
-	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
-	const isDesktop = useMediaQuery("(min-width: 1060px)");
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
 
-	useEffect(() => {
-		const handleScroll = () => {
-			if (window.scrollY === 0) {
-				setIsTopOfPage(true)
-				setSelectedPage("home")
-			}
-			if (window.scrollY !== 0) setIsTopOfPage(false);
-		};
-		window.addEventListener("scroll", handleScroll);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
 
-		return () => window.removeEventListener("scroll", handleScroll);
-	}, []);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-	return (
-		<div className='app bg-deep-blue'>
-			<NavBar
-				selectedPage={selectedPage}
-				setSelectedPage={setSelectedPage}
-				isTopOfPage={isTopOfPage}
-			/>
-			<div className='w-5/6 mx-auto md:h-full'>
-				{isDesktop && (
-					<DotGroup
-						selectedPage={selectedPage}
-						setSelectedPage={setSelectedPage}
-					/>
-				)}
-				<motion.div onViewportEnter={() => setSelectedPage("home")}>
+  return (
+    <div className='app bg-deep-blue'>
+      <NavBar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
+      />
+      <div className='w-5/6 mx-auto'>
+        {isDesktop && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <motion.div onViewportEnter={() => setSelectedPage("home")}>
           <Landing setSelectedPage={setSelectedPage} />
         </motion.div>
-			</div>
-			<LineGradient width={"w-full"} />
-			<div className='w-5/6 mx-auto md:h-full'>
-				<motion.div onViewportEnter={() => setSelectedPage("about")}>
+      </div>
+      <LineGradient width={"w-full"} />
+      <div className='w-5/6 mx-auto'>
+        <motion.div onViewportEnter={() => setSelectedPage("about")}>
           <About />
         </motion.div>
-			</div>
-			<LineGradient width={"w-full"} />
-			<div className='w-5/6 mx-auto md:h-full'>
-				<motion.div onViewportEnter={() => setSelectedPage("skills")}>
-					<Skills />
-				</motion.div>
-			</div>
-			<LineGradient width={"w-full"} />
-			<div className='w-5/6 mx-auto md:h-full'>
-				<motion.div onViewportEnter={() => setSelectedPage("projects")}>
-					<Projects />
-				</motion.div>
-			</div>
+      </div>
       <LineGradient width={"w-full"} />
-			<div className='w-5/6 mx-auto md:h-full'>
-				<motion.div onViewportEnter={() => setSelectedPage("contact")}>
-					<Contact />
-				</motion.div>
-			</div>
-		</div>
-	);
+      <div className='w-5/6 mx-auto'>
+        <motion.div onViewportEnter={() => setSelectedPage("skills")}>
+          <Skills />
+        </motion.div>
+      </div>
+      <LineGradient width={"w-full"} />
+      <div className='w-5/6 mx-auto'>
+        <motion.div onViewportEnter={() => setSelectedPage("projects")}>
+          <Projects />
+        </motion.div>
+      </div>
+      <LineGradient width={"w-full"} />
+      <div className='w-5/6 mx-auto'>
+        <motion.div onViewportEnter={() => setSelectedPage("contact")}>
+          <Contact />
+        </motion.div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
